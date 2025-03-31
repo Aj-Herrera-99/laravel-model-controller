0dea9c2e0034d2904f3990dcbf60fbc3a589365a
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-03-31 22:45:03 +0200

    db connection, movies retrieve

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-     @vite('resources/css/app.css')
+    @vite('resources/css/app.css')
 
-    <title>Homepage</title>
+    <title>@yield('title')</title>
 </head>
+
 <body>
-    <h1 class="text-5xl text-center">Tailwind installed</h1>
+    @yield('content')
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name("page1");
+// Route::get('/', function () {
+//     return view('home');
+// })->name("page1");
+Route::get("/", [PageController::class, 'index']);
